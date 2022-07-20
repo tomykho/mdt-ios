40f26440ef80dae33ca709220da576a5709db945
Tomy Kho <mail@tomykho.dev>
2022-07-20 14:30:10 +0700

    Initial Commit

---
+++ Shared/MDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDTApp.swift
+//  Shared
+//
+//  Created by Tomy Kho on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MDTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
